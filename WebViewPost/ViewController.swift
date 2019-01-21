036233b5ece790be9c2d54b985c8a0f4abe73edc
Pavan Powani <pavan.powani@practo.com>
2019-01-21 15:54:46 +0530

    Initial commit

--- WebViewPost/ViewController.swift
+++ WebViewPost/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        webView.load(postTodos())
+    }
+
+    func getTodos() -> URLRequest {
+        let mockURL = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
+        return URLRequest(url: mockURL)
+    }
+
+    func postTodos() -> URLRequest {
+        let mockURL = URL(string: "https://jsonplaceholder.typicode.com/todos/")!
+        let body = String(format: "title=%@&body=%@&userId=%@", "pjp288", "Pavan J Powani", "1")
+        var request = URLRequest(url: mockURL)
+        request.httpMethod = "POST"
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = body.data(using: .ascii, allowLossyConversion: true)!
+
+        return request
     }
 
 
